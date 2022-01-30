a15cbef56c3cc09a53a84fcd0812d0340e715e9e
Muhammad Rizwan <muhammad.rizwan@Techsurgeinc.com>
2022-01-30 13:54:59 +0500

    Footer Completed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import FooterContainer from "./containers/footer";
 import JumbotronContainer from "./containers/jumbotron"
 function App() {
   return (
-    <JumbotronContainer />
+    <>
+      <JumbotronContainer />
+      <FooterContainer />
+    </>
   );
 }
 
